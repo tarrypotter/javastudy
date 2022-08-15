29d77c0118ee1ca49e48db60380de3edfd9ad044
tarrypotter <109096999+tarrypotter@users.noreply.github.com>
2022-08-15 17:47:15 +0900

    Add files via upload

---
+++ CreateStringObject2.java
@@ -0,0 +1,20 @@
+
+
+public class CreateStringObject2 {
+
+	public static void main(String[] args) {
+		// 1
+		String str1 = new String("뭘봐");
+		System.out.println(str1);
+		
+		//2
+		String str2 = "뭐 어쩌라고";
+		System.out.println(str2);
+		
+		//걍 해본거
+		String str3 = "비둘기 구구구 ";
+		System.out.println(str3);
+
+	}
+
+}
