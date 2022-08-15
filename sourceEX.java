01c3dca486389facecb21bd5180117c62f494eb4
tarrypotter <109096999+tarrypotter@users.noreply.github.com>
2022-08-15 17:17:28 +0900

    Add files via upload

---
+++ sourceEX.java
@@ -0,0 +1,12 @@
+public class sourceEX{
+    public static void main (String[]args){
+        // 복사할때고 쓸려고 만들어놓은거
+        int[]a = new int[3];
+        a[0]=0; a[1]=1; a[2]=2; 
+        System.out.println(a[0]); 
+        System.out.println(a[1]); 
+        System.out.println(a[2]); 
+        
+
+  }
+}
