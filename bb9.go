17e7708b1ba971ccb6f00ac123c8ae92eb1751de
tarrypotter <109096999+tarrypotter@users.noreply.github.com>
2023-03-20 12:00:51 +0900

    M
    / /e@ 다라
    
    Signed-off-by: tarrypotter <109096999+tarrypotter@users.noreply.github.com>

---
+++ bb9.go
@@ -0,0 +1,18 @@
+//code app coding test
+
+//패키지 선언
+package main
+
+//패키지 가져오기
+import "fmt"
+
+//힘수를 선언하고 중괄호로 시작을 알림
+func bb9(){
+    fmt.Println("molra bidulgiya babmukja 999 ")
+    fmt.Println("아무래도 함수 이름과 똑같아야 하나봄 ㄷㄷ 그나저나 codeapp이 달라졌다!")
+}
+// main함수를 정의 (main패키지를 선언했는데 main함수가 정의되지않았을경우)
+// 아무래도 함수 이름과 똑같아야 하나보다 bb9 bb9 흠 ㅋㅋ
+func main(){
+    bb9()
+}
