086ead8ab9d8bcf0eb63b13b21d8e0065348e7c1
tarrypotter <109096999+tarrypotter@users.noreply.github.com>
2023-04-05 17:21:53 +0900

    Add files via upload
    Signed-off-by: tarrypotter <109096999+tarrypotter@users.noreply.github.com>

---
+++ TESTGROOMIDE.java.java
@@ -0,0 +1,15 @@
+//구름IDE TEST용
+package project;
+
+public class TESTGROOMIDE {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+         
+        System.out.println("구름ide 테스트 성공");
+    }
+
+}
